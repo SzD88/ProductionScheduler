1d7b34ffe9b9e2507b360359536a94ae03b0abc7
John Szop <szopakleks@hotmail.com>
2022-11-21 21:16:53 +0100

    234

---
+++ MachineReservations.Tests.Unit/Framework/ServiceCollectionTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace MachineReservations.Tests.Unit.Framework
+{
+    public class ServiceCollectionTest
+    {
+        [Fact]
+        public void test()
+        {
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddTransient<IMessanger, Messanger>();
+
+            var servicePovider = serviceCollection.BuildServiceProvider();
+
+            var messanager = servicePovider.GetRequiredService< IMessanger>();
+
+            messanager.ShouldNotBe(null);
+        }
+
+            private interface IMessanger
+        {
+            void Send();
+        }
+        private class Messanger : IMessanger
+        {
+            private readonly Guid _id = Guid.NewGuid();
+            public void Send()
+            {
+                Console.WriteLine("zz");
+            }
+        }
+    }
+
+
+}
+ 
