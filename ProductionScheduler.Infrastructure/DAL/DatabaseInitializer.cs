fdbcc7d35faf77b1a7b9424d2a44cba32cb20432
S.D <szopakleks@gmail.com>
2023-01-12 08:22:31 +0100

    123

--- ProductionScheduler.Infrastructure/DAL/DatabaseInitializer.cs
+++ ProductionScheduler.Infrastructure/DAL/DatabaseInitializer.cs
@@ -7,28 +7,24 @@ using ProductionScheduler.Core.ValueObjects;
 
 namespace ProductionScheduler.Infrastructure.DAL
 {
-    internal sealed class DatabaseInitializer : IHostedService
-        //background operations #21
+    internal sealed class DatabaseInitializer : IHostedService // BackgroundService, - operations in background while true -> do smth every 5 min for example
+                             //background operations #21
     {
         private readonly IServiceProvider _serviceProvider;
         public DatabaseInitializer(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
-        public Task StartAsync(CancellationToken cancellationToken)
+        public Task StartAsync(CancellationToken cancellationToken) // on app start
         {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<ProductionSchedulerDbContext>();
                 dbContext.Database.Migrate();
 
-                var periodMachineReservations = dbContext.PeriodMachineReservations.ToList();
 
-                if (!periodMachineReservations.Any()) // #refactor - shouldnt it check if number of machines is 
-                    //equal to 5 or new created sum of machines?
-                {
-                    var clock = new Clock();
-                    periodMachineReservations = new List<PeriodMachineReservation>()
+                var clock = new Clock();
+                var  expectedMachinesTable = new List<PeriodMachineReservation>()
                     {
 
                        new(Guid.Parse("00000000-0000-0000-0000-000000000001"), new ReservationTimeForward(clock.Current()), "P1"),
@@ -37,11 +33,25 @@ namespace ProductionScheduler.Infrastructure.DAL
                        new(Guid.Parse("00000000-0000-0000-0000-000000000004"), new ReservationTimeForward(clock.Current()), "P4"),
                        new(Guid.Parse("00000000-0000-0000-0000-000000000005"), new ReservationTimeForward(clock.Current()), "P5")
                     };
+
+
+                var periodMachineReservations = dbContext.PeriodMachineReservations.ToList();
+
+                if (!periodMachineReservations.Any() || periodMachineReservations.Count < expectedMachinesTable.Count) // #refactor - shouldnt it check if number of machines is 
+                    //equal to 5 or new created sum of machines?
+                {
+                    //  var clock = new Clock();
+
+                    foreach (var item in periodMachineReservations)
+                    {
+                        dbContext.Remove(item);
+                    }
+
+                    periodMachineReservations = expectedMachinesTable;
                     
                     dbContext.AddRange(periodMachineReservations);
                     dbContext.SaveChanges();
-                }
-
+                } 
             }
             return Task.CompletedTask;
         }
