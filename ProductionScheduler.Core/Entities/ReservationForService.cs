b73ffdd7c8da33452fc75820a56ac675fb6139ec
S.D <szopakleks@gmail.com>
2023-02-09 11:54:58 +0100

    123

---
+++ ProductionScheduler.Core/Entities/ReservationForService.cs
@@ -0,0 +1,17 @@
+﻿using ProductionScheduler.Core.ValueObjects;
+
+namespace ProductionScheduler.Core.Entities
+{
+    public sealed class ReservationForService : Reservation
+    {
+        private ReservationForService()
+        {
+
+        }
+        public ReservationForService(ReservationId id, MachineId machineId, 
+          Date date, Hour hour) 
+            : base (id, machineId, hour, date)
+        {
+        }
+    }
+}
