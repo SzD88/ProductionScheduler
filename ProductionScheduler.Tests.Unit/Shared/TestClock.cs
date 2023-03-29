122f98c964ea4e31bacbd010d9cd310fb7a8001c
John Szop <szopakleks@hotmail.com>
2023-03-29 19:24:14 +0200

    x

--- ProductionScheduler.Tests.Unit/Shared/TestClock.cs
+++ ProductionScheduler.Tests.Unit/Shared/TestClock.cs
@@ -6,8 +6,7 @@ namespace MachineReservations.Tests.Unit.Shared
     public class TestClock : IClock
     {
         public DateTime Current()
-        {
-          // return new DateTime(2023, 01, 17, 12, 0, 0);
+        { 
             return DateTime.UtcNow.AddDays(0);
         }
     }
